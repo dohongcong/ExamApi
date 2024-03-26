b2eb683d1ed02ecbfe638bebb47621a20bdb2d4d
dohongcong <congdhth2208072@fpt.edu.vn>
2024-03-26 09:54:48 +0700

    Api
    exam

---
+++ ExamApi/ExamApi/Data/ExamApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ExamApi.Models;
+
+namespace ExamApi.Data
+{
+    public class ExamApiContext : DbContext
+    {
+        public ExamApiContext (DbContextOptions<ExamApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ExamApi.Models.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ ExamApi/ExamApi/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace ExamApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Names { get; set; }
+        public string Department { get; set; }
+    }
+}
